e63aff1bb5d16d66f580acb79ca75fb589e84fba
Seiji Emery <seijiemery@gmail.com>
2019-05-22 20:38:32 -0700

    glium window

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,29 @@
+#[macro_use]
+extern crate glium;
+
+use glium::Surface;
+
 fn main() {
-    println!("Hello, world!");
+    use glium::glutin;
+    let mut events_loop = glutin::EventsLoop::new();
+    let wb = glutin::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+    let mut closed = false;
+    while !closed {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.finish().unwrap();
+
+        events_loop.poll_events(|ev| {
+            match ev {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::CloseRequested => closed = true,
+                    glutin::WindowEvent::Resized(sz) => println!("resized: {:?}", sz),
+                    _ => ()
+                },
+                _ => ()
+            }
+        });
+    }
 }
