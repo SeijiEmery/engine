475bafe4f03998bb9e4a0ba22833e29e93417dbc
Seiji Emery <seijiemery@gmail.com>
2019-05-23 21:27:38 -0700

    okay, that works - problem is with the transform

--- rs/src/render_backends/glium_renderer.rs
+++ rs/src/render_backends/glium_renderer.rs
@@ -71,7 +71,8 @@ impl ShapeRenderer {
             out vec2 local_coords;
             uniform mat4 transform;
             void main () {
-                gl_Position = transform * vec4(position, 0.0, 1.0);
+                gl_Position = vec4(position, 0.0, 1.0);
+//                gl_Position = transform * vec4(position, 0.0, 1.0);
                 local_coords = position;
             }
         "#;
