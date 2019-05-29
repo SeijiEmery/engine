731929e579a1e5f4eae15b753cd2dfec50b17dc6
Seiji Emery <seijiemery@gmail.com>
2019-05-28 20:51:39 -0700

    worked out how to run a glfw app...

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -1,6 +1,20 @@
-module Main where
+module Main (main) where
+import Graphics.UI.GLFW as GLFW
+import Control.Monad
+import Control.Conditional
 
-import Lib
+enforce :: Bool -> String -> IO ()
+enforce action msg = if action then return () else fail msg
+
+run_glfw :: IO () -> IO ()
+run_glfw body = init >> body >> teardown
+    where
+        init = do
+            ok <- GLFW.init
+            enforce ok "Failed to initialize GLFW window"
+        teardown = do
+            return ()
 
 main :: IO ()
-main = someFunc
+main = run_glfw $ do
+    print "Hello world!"
