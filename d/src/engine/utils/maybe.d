2efe5b7187b8e328052f6f980d1919f966e6ae3d
Seiji Emery <seijiemery@gmail.com>
2019-05-29 18:54:40 -0700

    finished maybe impl

--- d/src/engine/utils/maybe.d
+++ d/src/engine/utils/maybe.d
@@ -1,15 +1,24 @@
 module engine.utils.maybe;
 
 struct Maybe (T) {
-    import std.variant;
-    private alias MaybeVariant = Algebraic!T;
-    private MaybeVariant value;
-    alias value this;
+    // use d's brilliant std.variant.Algebraic impl by andrei alexandrescu
+    import std.variant: Algebraic;
+    private Algebraic!T value;
 
-    this (T value) { this.value = value; }
+    // implicit default ctor => Nothing
+    // value ctor (T value)  => Just(value)
+    static if (!(is(T == void))) {
+        this (T value) { this.value = value; }
+    }
 
+    // check iff Some (Just) or None (Nothing)
     bool isSome () { return this.value.hasValue; }
     bool isNone () { return !this.value.hasValue; }
+
+    // equivalent to haskell's fmap, rust's Option.map:
+    // Just(x).map(f) => Just(f(x))
+    // Nothing.map(f) => Nothing
+    // note: map returns void iff f returns void
     auto map (F)(F f) {
         import std.traits: ReturnType;
         alias R = ReturnType!F;
@@ -20,12 +29,74 @@ struct Maybe (T) {
             return Maybe!R();
         }
     }
-    auto ref setDefault (T defaultValue) {
-        if (isNone) { value = defaultValue; }
-        return this;
+
+    // Non-safely get value directly (throws an exception if is None)
+    static if (!(is(T == void))) {
+        T unwrap () { return value.get!T; }
     }
-    T withDefault (T defaultValue) {
-        if (isSome) return *value.peek!T;
-        return defaultValue;
+
+    // Safely get value OR an optional default value
+    static if (!(is(T == void))) {
+        T withDefault (T defaultValue) {
+            if (isSome) return *value.peek!T;
+            return defaultValue;
+        }
     }
 }
+auto maybe (T)(T value) { return Maybe!T(value); }
+auto maybe (T)() { return Maybe!T(); }
+auto just (T)(T value) { return Maybe!T(value); }
+auto nothing (T = void)() { return Maybe!T(); }
+
+
+unittest {
+    import std.format: format;
+    import std.exception: assertThrown;
+    import std.variant: VariantException;
+    import std.conv: to;
+
+    // Just(10), T == int
+    assert(is(typeof(just(10)) == Maybe!int));
+    assert(!just(10).isNone);
+    assert(just(10).isSome);
+    assert(is(typeof(just(10).unwrap()) == int));
+    assert(just(10).unwrap == 10);
+
+    // Nothing (but with T == int)
+    assert(is(typeof(nothing!int()) == Maybe!int));
+    assert(nothing!int().isNone);
+    assert(!nothing!int().isSome);
+    assert(is(typeof(nothing!int().unwrap()) == int));
+
+    // calling unwrap() on a nothing value raises an exception
+    assertThrown!VariantException(nothing!int().unwrap);
+
+    // special case: this is a Maybe!void
+    assert(is(typeof(nothing()) == Maybe!void));
+    assert(nothing().isNone);
+    assert(!nothing().isSome);
+
+    // unwrap() is undefined on Maybe!void!
+    assert(!__traits(compiles, nothing().unwrap));
+    assert(__traits(compiles, just(10).unwrap));
+    assert(__traits(compiles, nothing!int().unwrap));
+
+    // test .map()
+    auto x = 0, y = 0;
+    //assert(is(typeof(just(10).map((v) { x = v; })) == void));
+    //assert(is(typeof(just(10).map((v) => v)) == int));
+    //assert(is(typeof(just(10).map((v) => v.to!string)) == string));
+
+    // test running void functions with side effects
+    assert(x == 0 && y == 0);
+    just(10).map((int v) { x = v; });
+    nothing!int().map((int v) { y = v; });
+    assert(x == 10 && y == 0);
+
+    // test running normal functions
+    assert(just(10).map((int v) => v).withDefault(0) == 10);
+    assert(just(10).map((int v) => v.to!string).withDefault("") == "10");
+    assert(nothing!int.map((int v) => v).withDefault(0) == 0);
+    assert(nothing!int.map((int v) => v.to!string).withDefault("") == "");
+}
+
