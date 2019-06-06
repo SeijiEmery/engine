06ad45c67410db62a8a826753d9fac571280430e
Seiji Emery <seijiemery@gmail.com>
2019-06-06 00:24:06 -0700

    cleanup

--- d/examples/test_systemic/main.d
+++ d/examples/test_systemic/main.d
@@ -75,6 +75,28 @@ SystemicFunction systemic (SystemicParams params, string bodyImpl)() {
     };
 }
 
+mixin template generate_systemic (SystemicParams params, string bodyImpl) {
+    shared static this () {
+        void systemic_impl (ref EntityManager entities, ref SystemsGlobalResourceManager resources) {
+            mixin(systemic_body_impl(params, bodyImpl));
+        }
+        g_system_builders ~= delegate () {
+            register_system!systemic_impl(systemic_typesig(params));
+        };
+    }
+}
+void register_system (alias systemic_function)(string id) {
+    g_registered_systems[id] = &systemic_function;
+}
+void registerSystems () {
+    foreach (builder; g_system_builders) {
+        builder();
+    }
+}
+alias SystemBuilder = void delegate ();
+shared static SystemicFunction[string] g_registered_systems;
+shared static SystemBuilder[] g_system_builders;
+
 void run_tests (SystemicParams stuff)() {
     enum tsig = systemic_typesig(stuff);
     enum tbody = systemic_body_impl(stuff, q{rot.angle += r.speed * dt;});
@@ -94,106 +116,20 @@ void run_tests (SystemicParams stuff)() {
     writefln("%s, %s", entity.component!RotationAngle.angle, resources.get!DeltaTime);
 }
 
-//mixin generate_systemic!(
-//    systemic_typelist!(
-//        systemic_typedecl!("Rotator", "r", SystemicParam.In, SystemicResourceType.Component),
-//        systemic_typedecl!("DeltaTime", "dt", SystemicParam.In, SystemicResourceType.Singleton),
-//        systemic_typedecl!("RotationAngle", "angle", SystemicParam.Out, SystemicResourceType.Component),
-//    ), "
-//        angle.angle += r.speed * dt.dt;
-//    "
-//);
+mixin generate_systemic!([
+        tuple("Rotator", "r", SystemicParam.In, SystemicResourceType.Component),
+        tuple("DeltaTime", "dt", SystemicParam.In, SystemicResourceType.Singleton),
+        tuple("RotationAngle", "rot", SystemicParam.InOut, SystemicResourceType.Component),
+    ], q{rot.angle += r.speed * dt;});
 
-struct systemic_typedecl (string tname, string vname, SystemicParam inp) {
-    alias type  = tname;
-    alias var   = vname;
-    alias input = inp;
-}
-struct systemic_typelist (Args...) {
-    alias args = Args;
-}
-string eparams (Args...)() {
-    static if (Args.length == 0) return "";
-    else static if (Args.length == 1) {
-        return Args[0].var;
-        //static if (Args[0].input == SystemicParam.In) return "const "~Args[0].var~"";
-        //else return Args[0].var;
-        //else return "ref "~Args[0].var;
-    }
-    else return eparams!(Args[0..1]) ~ ", " ~ eparams!(Args[1..$]);
-}
-string tparams (Args...)() {
-    static if (Args.length == 0) return "";
-    else static if (Args.length == 1) {
-        return Args[0].type;
-        //static if (Args[0].input == SystemicParam.In) return "const(" ~ Args[0].type ~ ")";
-        //else return Args[0].type;
-        //else return "ref "~Args[0].type;
-    }
-    else return tparams!(Args[0..1]) ~ ", " ~ tparams!(Args[1..$]);
-}
-string tparams_in (Args...)() {
-    static if (Args.length == 0) return "";
-    else static if (Args[0].input == SystemicParam.Out) return tparams_in!(Args[1..$]);
-    else static if (Args.length == 1) return Args[0].type;
-    else {
-        auto a = Args[0].type;
-        auto b = tparams_in!(Args[1 .. $]);
-        return b != "" ? a ~ ", " ~ b : a;
-    }
-}
-string tparams_out (Args...)() {
-    static if (Args.length == 0) return "";
-    else static if (Args[0].input == SystemicParam.In) return tparams_out!(Args[1..$]);
-    else static if (Args.length == 1) return Args[0].type;
-    else {
-        auto a = Args[0].type;
-        auto b = tparams_out!(Args[1 .. $]);
-        return b != "" ? a ~ ", " ~ b : a;
-    }
-}
-string tsignature (Args...)() {
-    auto left = tparams_in!Args;
-    auto right = tparams_out!Args;
-    return left != "" ?
-        (right != "" ? left ~ " -> " ~ right : left)
-        //(right != "" ? right : "")
-        : right;
-}
-string systemic_body (alias T, alias B)() {
-    return "foreach (entity, " ~ eparams!(T.args) ~ "; entities.entitiesWith!(" ~ tparams!(T.args) ~ ")) { " ~ B ~ " }";
-}
-alias SystemFunction = void delegate (ref EntityManager, ref SystemsGlobalResourceManager);
-shared static SystemFunction[string] g_registered_systems;
 
-alias SystemBuilder = void delegate ();
-shared static SystemBuilder[] g_system_builders;
-
-mixin template generate_systemic (alias T, alias B) {
-    shared static this () {
-        void systemic_impl (ref EntityManager entities, ref SystemsGlobalResourceManager resources) {
-            mixin(systemic_body!(T, B));
-        }
-        g_system_builders ~= delegate () {
-            register_system!systemic_impl(tsignature!(T.args));
-        };
-    }
-}
-void register_system (alias systemic_function)(string id) {
-    g_registered_systems[id] = &systemic_function;
-}
 void main () {
-    //foreach (name, sys; g_registered_systems) {
-    //    writefln("have system '%s': %p", name, cast(void*)sys);
-    //}
-    //alias stuff = systemic_typelist!(
-    //    systemic_typedecl!("Rotator", "r", SystemicParam.In),
-    //    systemic_typedecl!("DeltaTime", "dt", SystemicParam.In),
-    //    systemic_typedecl!("RotationAngle", "angle", SystemicParam.InOut),
-    //);
-    //enum result = eparams!(stuff.args);
-    //enum tres = tsignature!(stuff.args);
-    //writefln("%s :: %s", result, tres);
+    registerSystems();
+    writefln("have %s system(s):", g_registered_systems.length);
+    foreach (name, sys; g_registered_systems) {
+        writefln("  %s", name);
+    }
+    writefln("testing...");
     run_tests!([
         tuple("Rotator", "r", SystemicParam.In, SystemicResourceType.Component),
         tuple("DeltaTime", "dt", SystemicParam.In, SystemicResourceType.Singleton),
